2267c29dd5e452f0dfa4f3abba37405fb500accd
Rajesh-107 <applerb326@gmail.com>
2022-05-10 11:56:38 +0600

    navbar setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Navbar from './Pages/Sherad/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
