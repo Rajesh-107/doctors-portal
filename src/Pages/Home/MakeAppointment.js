cb73855af229c7cf99faa86b44d64fe58f9e5f36
Rajesh-107 <applerb326@gmail.com>
2022-05-11 23:16:53 +0600

    add appointment

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -11,7 +11,7 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-100px] w-full' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold py-2 px-2'>Appointment</h3>
                 <h2 className='text-3xl text-white py-3 px-2'>Make an Appointment Today</h2>
                 <p className='text-white py-3 px-2'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa tenetur perferendis nesciunt molestiae? Autem dolore quia a debitis. Quis, deleniti? Atque vitae dicta nam. Impedit minima quasi reprehenderit cumque facere Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, fugit!.</p>
