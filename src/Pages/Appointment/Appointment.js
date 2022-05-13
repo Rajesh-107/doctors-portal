fdeb187a9d289cf9b45745250f6f2f6e815ce4f6
Rajesh-107 <applerb326@gmail.com>
2022-05-13 12:08:44 +0600

    added calendar

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Footer from '../Sherad/Footer';
+import AppointmentBanner from './AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
-            
+            <AppointmentBanner></AppointmentBanner>
             <Footer></Footer>
         </div>
     );
